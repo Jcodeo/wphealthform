85ed77f9984a536f407b0c484649bb167adce05c
Jcodeo <harsondev@gmail.com>
2022-08-25 22:19:46 +0300

    shortcode generation for the health form

--- wphealthform.php
+++ wphealthform.php
@@ -37,14 +37,25 @@ if ( ! class_exists( 'WpHealthForm' ) ) {
         }
 
         public function enqueue_front() {
+            wp_enqueue_style( 'bootstrap' , plugins_url( 'assets/css/libs/bootstrap.min.css', __FILE__ ), [], '4.5.3', 'all' );
             wp_enqueue_style( 'wphealthform-style', plugins_url( 'assets/css/wphealthform.css', __FILE__ ), [], time(), 'all' );
             wp_enqueue_script( 'wphealthform-script', plugins_url( 'assets/js/wphealthform.js', __FILE__ ), ['jquery'], time(), true );
+        }
+
+        public function register_shortcode() {
+            add_shortcode( 'wp_health_form', [ $this, 'generate_shortcode' ] );
+        }
+        public function generate_shortcode() {
+            ob_start();
+            require_once(plugin_dir_path( __FILE__ ) . 'templates/front/healthform.php');
+            return ob_get_clean();
         }
 
     }
 
     $wpHealthForm = new WpHealthForm();
     $wpHealthForm->register_assets();
+    $wpHealthForm->register_shortcode();
 
     register_activation_hook( __FILE__, [$wpHealthForm, 'activate'] );
 
