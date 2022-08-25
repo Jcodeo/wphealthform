5813452d1b3f9a4bf8c4009372bbe0f3bdc00cdf
Jcodeo <harsondev@gmail.com>
2022-08-25 21:14:31 +0300

    create plugin main file

---
+++ wphealthform.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package WpHealthForm
+ */
+/*
+Plugin Name: WP Health Form
+Plugin URI: #
+Description: Ce plugin permet d'ajouter un formulaire de santé permettant d'obtenir un sondage auprès des internautes.
+Version: 1.0
+Author: Harson Jr
+Author URI: https://www.linkedin.com/in/harson-joro-jr007/
+Licence: GPLv2 or later
+Text Domain: wphealthform
+*/
